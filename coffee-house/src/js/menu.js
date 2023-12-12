199334443bef35503b92980392b71559a6fd41dc
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2023-12-12 14:11:38 +0300

    fix: correct change of behavior when navigating through menu items

--- coffee-house/src/js/menu.js
+++ coffee-house/src/js/menu.js
@@ -2,8 +2,10 @@ const navigation = document.querySelector('.navigation');
 const burgerBtn = document.querySelector('.header__burger');
 const burgerLines = document.querySelectorAll('.header__burger-line');
 const navigationItems = document.querySelectorAll('.navigation__item');
+const body = document.querySelector('body');
 
 burgerBtn.addEventListener('click', () => {
+    body.classList.toggle('fix-screen');
     navigation.classList.toggle('navigation--open');
     burgerBtn.classList.toggle('header__burger--active');
     burgerLines.forEach(item => {
@@ -14,6 +16,7 @@ burgerBtn.addEventListener('click', () => {
 
 navigationItems.forEach(item => {
     item.addEventListener('click', () => {
+        body.classList.toggle('fix-screen');
         navigation.classList.toggle('navigation--open');
         burgerBtn.classList.toggle('header__burger--active');
         burgerLines.forEach(item => {
