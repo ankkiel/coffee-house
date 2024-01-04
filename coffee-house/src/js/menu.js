7a19f6ddd56442cabcbadf2738e0e5c435e81ba1
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2024-01-04 15:16:41 +0300

    refactor: change the opening menu

--- coffee-house/src/js/menu.js
+++ coffee-house/src/js/menu.js
@@ -4,23 +4,17 @@ const burgerLines = document.querySelectorAll('.header__burger-line');
 const navigationItems = document.querySelectorAll('.navigation__item');
 const body = document.querySelector('body');
 
-burgerBtn.addEventListener('click', () => {
+const toggleNavigation = () => {
     body.classList.toggle('fix-screen');
     navigation.classList.toggle('navigation--open');
     burgerBtn.classList.toggle('header__burger--active');
     burgerLines.forEach(item => {
         item.classList.toggle('header__burger-line--active');
-    })
-});
+    });
+};
 
+burgerBtn.addEventListener('click', toggleNavigation);
 
 navigationItems.forEach(item => {
-    item.addEventListener('click', () => {
-        body.classList.toggle('fix-screen');
-        navigation.classList.toggle('navigation--open');
-        burgerBtn.classList.toggle('header__burger--active');
-        burgerLines.forEach(item => {
-            item.classList.toggle('header__burger-line--active');
-        })
-    })
+    item.addEventListener('click', toggleNavigation);
 });
