58a2f7cc16bff93089a0656e152b43a17e0d8b31
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2024-01-11 22:02:49 +0300

    fix: correct mistakes in console

--- coffee-house/src/js/category.js
+++ coffee-house/src/js/category.js
@@ -1,107 +1,112 @@
-class Menu {
-  constructor() {
-    this.currentCategory = 'coffee';
-    this.tabs = document.querySelectorAll('.tabs__item');
-    this.init();
-  }
-  // Инициализация
-  async init() {
-    const { default: products } = await import('./products.json');
-    this.productArray = Object.values(products);
-
-    this.tabs.forEach((tab) => {
-      tab.addEventListener('click', () => {
-        this.currentCategory = tab.dataset.category;
-        this.updateTabs();
-        this.updateMenuProducts();
-      });
-    });
-
-    this.updateMenuProducts();
-  }
-  // Обновляем табы
-  updateTabs() {
-    this.tabs.forEach((tab) => {
-      if (tab.dataset.category === this.currentCategory) {
-        tab.classList.add('tabs__item--active');
-      } else {
-        tab.classList.remove('tabs__item--active');
+(function() {
+  const menu = document.querySelector('.menu');
+  if (menu) {
+    class Menu {
+      constructor() {
+        this.currentCategory = 'coffee';
+        this.tabs = document.querySelectorAll('.tabs__item');
+        this.init();
       }
-    });
-  }
-
-  updateMenuProducts() {
-    const menuProducts = document.querySelector('.menu__products');
-    // Удаляем все существующие карточки продуктов
-    menuProducts.innerHTML = '';
+      // Инициализация
+      async init() {
+        const { default: products } = await import('./products.json');
+        this.productArray = Object.values(products);
   
-    // Фильтруем продукты по выбранной категории
-    const filteredProducts = this.productArray.filter(
-      (product) => product.category === this.currentCategory
-    );
+        this.tabs.forEach((tab) => {
+          tab.addEventListener('click', () => {
+            this.currentCategory = tab.dataset.category;
+            this.updateTabs();
+            this.updateMenuProducts();
+          });
+        });
   
-    // Определяем, сколько элементов отображать на планшете
-    const maxItems = window.innerWidth < 768 ? 4 : filteredProducts.length;
-
-    const refreshBtn = document.querySelector('.menu__refresh');
+        this.updateMenuProducts();
+      }
+      // Обновляем табы
+      updateTabs() {
+        this.tabs.forEach((tab) => {
+          if (tab.dataset.category === this.currentCategory) {
+            tab.classList.add('tabs__item--active');
+          } else {
+            tab.classList.remove('tabs__item--active');
+          }
+        });
+      }
   
-    // Создаем карточки продуктов только для отфильтрованных продуктов и добавляем их в обертку
-    filteredProducts.slice(0, maxItems).forEach((product, idx) => {
-      const menuCard = this.createMenuCard(product, idx);
-      menuProducts.appendChild(menuCard);
-    });
+      updateMenuProducts() {
+        const menuProducts = document.querySelector('.menu__products');
+        // Удаляем все существующие карточки продуктов
+        menuProducts.innerHTML = '';
+      
+        // Фильтруем продукты по выбранной категории
+        const filteredProducts = this.productArray.filter(
+          (product) => product.category === this.currentCategory
+        );
+      
+        // Определяем, сколько элементов отображать на планшете
+        const maxItems = window.innerWidth < 768 ? 4 : filteredProducts.length;
   
-    if (filteredProducts.length > maxItems) {
-      refreshBtn.addEventListener('click', () => {
-        // Добавляем оставшиеся элементы
-        filteredProducts.slice(maxItems).forEach((product, idx) => {
-          const menuCard = this.createMenuCard(product, idx + maxItems);
+        const refreshBtn = document.querySelector('.menu__refresh');
+      
+        // Создаем карточки продуктов только для отфильтрованных продуктов и добавляем их в обертку
+        filteredProducts.slice(0, maxItems).forEach((product, idx) => {
+          const menuCard = this.createMenuCard(product, idx);
           menuProducts.appendChild(menuCard);
         });
-        // Удаляем кнопку
-        refreshBtn.remove();
-      });
-    }
-  }
+      
+        if (filteredProducts.length > maxItems) {
+          refreshBtn.addEventListener('click', () => {
+            // Добавляем оставшиеся элементы
+            filteredProducts.slice(maxItems).forEach((product, idx) => {
+              const menuCard = this.createMenuCard(product, idx + maxItems);
+              menuProducts.appendChild(menuCard);
+            });
+            // Удаляем кнопку
+            refreshBtn.remove();
+          });
+        }
+      }
+      
+      createMenuCard(product, idx) {
+        // Создаем карточку продукта
+        const menuCard = document.createElement('a');
+        menuCard.classList.add('menu__card', 'menu-card');
+        menuCard.href = '#';
   
-  createMenuCard(product, idx) {
-    // Создаем карточку продукта
-    const menuCard = document.createElement('a');
-    menuCard.classList.add('menu__card', 'menu-card');
-    menuCard.href = '#';
-
-    // Добавляем изображение продукта
-    const image = document.createElement('div');
-    image.classList.add('menu-card__image');
-    const img = document.createElement('img');
-    img.src = new URL(`../../assets/images/${product.category}-${idx + 1}.png`, import.meta.url)
-    img.alt = product.name;
-    image.appendChild(img);
-    menuCard.appendChild(image);
-
-    // Добавляем название и описание продукта
-    const body = document.createElement('div');
-    body.classList.add('menu-card__body');
-
-    const title = document.createElement('h3');
-    title.classList.add('menu-card__title');
-    title.textContent = product.name;
-
-    const description = document.createElement('p');
-    description.classList.add('menu-card__description');
-    description.textContent = product.description;
-
-    const price = document.createElement('span');
-    price.classList.add('menu-card__price');
-    price.textContent = `$${product.price}`;
-
-    body.appendChild(title);
-    body.appendChild(description);
-    body.appendChild(price);
-    menuCard.appendChild(body);
-
-    return menuCard;
+        // Добавляем изображение продукта
+        const image = document.createElement('div');
+        image.classList.add('menu-card__image');
+        const img = document.createElement('img');
+        img.src = new URL(`../../assets/images/${product.category}-${idx + 1}.png`, import.meta.url)
+        img.alt = product.name;
+        image.appendChild(img);
+        menuCard.appendChild(image);
+  
+        // Добавляем название и описание продукта
+        const body = document.createElement('div');
+        body.classList.add('menu-card__body');
+  
+        const title = document.createElement('h3');
+        title.classList.add('menu-card__title');
+        title.textContent = product.name;
+  
+        const description = document.createElement('p');
+        description.classList.add('menu-card__description');
+        description.textContent = product.description;
+  
+        const price = document.createElement('span');
+        price.classList.add('menu-card__price');
+        price.textContent = `$${product.price}`;
+  
+        body.appendChild(title);
+        body.appendChild(description);
+        body.appendChild(price);
+        menuCard.appendChild(body);
+  
+        return menuCard;
+      }
+    }
+  
+    new Menu();
   }
-}
-
-new Menu();
+})();
--- coffee-house/src/js/menu.js
+++ coffee-house/src/js/menu.js
@@ -1,20 +1,22 @@
-const navigation = document.querySelector('.navigation');
-const burgerBtn = document.querySelector('.header__burger');
-const burgerLines = document.querySelectorAll('.header__burger-line');
-const navigationItems = document.querySelectorAll('.navigation__item');
-const body = document.querySelector('body');
+(function() {
+    const navigation = document.querySelector('.navigation');
+    const burgerBtn = document.querySelector('.header__burger');
+    const burgerLines = document.querySelectorAll('.header__burger-line');
+    const navigationItems = document.querySelectorAll('.navigation__item');
+    const body = document.querySelector('body');
 
-const toggleNavigation = () => {
-    body.classList.toggle('fix-screen');
-    navigation.classList.toggle('navigation--open');
-    burgerBtn.classList.toggle('header__burger--active');
-    burgerLines.forEach(item => {
-        item.classList.toggle('header__burger-line--active');
+    const toggleNavigation = () => {
+        body.classList.toggle('fix-screen');
+        navigation.classList.toggle('navigation--open');
+        burgerBtn.classList.toggle('header__burger--active');
+        burgerLines.forEach(item => {
+            item.classList.toggle('header__burger-line--active');
+        });
+    };
+
+    burgerBtn.addEventListener('click', toggleNavigation);
+
+    navigationItems.forEach(item => {
+        item.addEventListener('click', toggleNavigation);
     });
-};
-
-burgerBtn.addEventListener('click', toggleNavigation);
-
-navigationItems.forEach(item => {
-    item.addEventListener('click', toggleNavigation);
-});
+})();
